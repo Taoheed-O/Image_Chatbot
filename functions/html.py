a832f887bd962a39ff9c33399fd1eeb30be03431
Taoheed Oyeniyi <64612602+Taoheed-O@users.noreply.github.com>
2024-04-12 08:34:49 +0100

    new update...

---
+++ functions/html.py
@@ -0,0 +1,121 @@
+ABOUT_APP = '''
+        <div class="nice-div">
+                        </br>
+            <h1> Bank Statement Analyzer</h1>
+            <h2> About: </h2>
+                This application interface is built with streamlit.
+                App Features include:
+                    <ul>
+                        <li> Visualizations. </li>
+                        <li> Reading pdf of bank statements and displaying the tabular or JSON format of the transactions. </li>
+                        <li> Flexibility in choosing views and amount of information. </li>
+                        <li> The app is built to handle different banks and account types. </li>
+                        <li> The app can handle both protected and non-protected pdfs. </li>
+                    </ul>
+                The streamlit library is used for building the interface of the app.
+                        <h2>Files and Folders:</h2>
+                There are multiple files and folders in this project:
+                <br/>
+                <b>FOLDERS</b>
+                    <li><b>functions</b></li>
+                        This folder contains all the necessary individual functions of the app:
+                            <ul>
+                                <li>html</li>
+                                    This function contains all the necessary html codes for the app interface.
+                                <li>display_table</li>
+                                    This function contains the codes for displaying the tables and visualizations of the app.
+                            </ul>
+                    <li><b>bank_statements_cleaner</b></li>
+                            This folder contains the necessary individual codes to extract the data for each available bank.
+                    <b>FILES</b>
+                    <ul>
+                        <li><b>main</b></li>
+                            This is the main file that contains the streamlit interface code and the calling of the various functions in the functions folder.
+                    </ul>
+                    <i>NB: Test the app automatically here:</i>
+                    <a href="https://share.streamlit.io/taoheed-o/bank_statement_analyzer/main/main.py" target="_blank">Test me..</a>
+                    <br/>
+                    <i> Check the full code on github:</i>
+                    <a href="https://github.com/Taoheed-O/Bank_Statement_Analyzer.git" target="_blank">Check me out</a>
+        </div>'''
+
+
+
+ABOUT_DEVELOPER = '''
+        <div class="nice-div">          
+                    <br/>   
+                    I am a passionate and results-driven Data Scientist with a strong background in analyzing complex data sets and deriving valuable insights to drive business growth. With a deep understanding of machine learning algorithms, and data visualization techniques,
+                    I thrive on solving intricate problems and uncovering hidden patterns to optimize decision-making processes.
+                    <br/>
+
+        <b> Education: </b>
+                    <br/>
+                    I am an Engineering student at the University of Ibadan. My academic journey has assisted in developing a solid foundation in mathematics, engineering, programming and problem solving skills, which I have further honed through hands-on experience in various real-world projects.
+
+        <b> Professional Experience: </b>
+                    <br/>
+                    I have more than 4 years of experience as a Data Scientist, working with diverse organizations as an intern. In my previous role at Zummit Africa, I successfully worked on several projects that involved developing predictive models and Natural language processing. My work directly contributed to the success of my team.
+
+        Furthermore, I collaborated closely with cross-functional teams, including data engineers and machine learning engineers, to work on numerous projects including Large Language Models and computer vision. My expertise in programming languages such as Python and SQL, coupled with my proficiency in data manipulation and analysis tools, allowed me to effectively extract, clean, and transform large datasets for analysis.
+
+        <b> Skills: </b>
+        <ul>
+                <li> command line </li>
+                <li> Data analysis and interpretation </li>
+                <li> Machine learning algorithms and predictive modeling </li>
+                <li> ComputerVision </li>
+                <li> Data visualization </li>
+                <li> Programming languages: Python, SQL </li>
+                <li> Data manipulation and analysis tools: Pandas, NumPy </li>
+                <li> Data Visualization tools: Matplotlib, seaborn </li>
+                <li> Deep learning framework:PyTorch </li>
+                <li> Data extraction and transformation </li>
+        </ul>
+
+        If you are seeking a dedicated and experienced Data Scientist who can bring valuable insights to your organization, feel free to reach out. I am eager to explore new opportunities, contribute to impactful projects, and be part of a team that shares a vision for harnessing the power of data to drive success.
+            </div>
+                '''
+
+
+STYLE = '''
+        <style>
+            .nice-div {
+            font-family: 'Pacifico', cursive;
+            font-size: 20px;
+            color: #003366;
+            padding: 10px;
+            margin: 20px;
+            border: 2px solid #003366;
+            border-radius: 10px;
+            }
+            .links {
+                display: flex;
+                font-family: 'Pacifico', cursive;
+                font-size: 20px;
+                color: #003366;
+                
+                height: 100vh;
+            }
+            a {
+                margin: 10px;
+                color: #000;
+                text-decoration: none;
+            }
+        </style>'''
+
+
+LINK = '''
+        <head>
+                <!-- Add font awesome icon library -->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        </head>   
+                <div class="links">
+                    <b> Follow/Reach out to me on:<b>
+                    <ul>
+                        <li><a href="https://github.com/Taoheed-O/" target="_blank"><i class="fa fa-github"></i>  Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/taoheed-oyeniyi/" target="_blank"><i class="fa fa-linkedin"></i>  LinkedIn</a></li>
+                    </ul>
+                </div>
+                '''
+
+
--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import streamlit as st
 import requests
 import io
 from PIL import Image
+from streamlit_option_menu import option_menu
+from functions import html
 
+
+
+style = html.STYLE
+about_dev = html.ABOUT_DEVELOPER
+about_app = html.ABOUT_APP
+link = html.LINK
 
 def query_stabilitydiff(payload, headers):
     API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
@@ -14,41 +22,76 @@ def query_stabilitydiff(payload, headers):
 # interface
 with st.sidebar:
     "[View the source code](https://github.com/Taoheed-O/Image_Chatbot)"
+    info = option_menu("Info", ["App", "About the app","About the Developer"],
+                    icons=['joystick', 'book', 'person lines fill'],
+                    menu_icon="cast", default_index=0,
+                    styles={
+                        "container": {"padding": "5!important", "background-color": "white"},
+                        "icon": {"color": "black", "font-size": "25px"},
+                        "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px",
+                                    "--hover-color": "orange"},
+                                    "nav-link-selected": {"background-color": "red", "color": "white", "font-size": "16px",
+                                                            "text-align": "left", "margin": "0px", "border-radius": "5px", "--hover-color": "orange"},
+                    }
+                    )
+    
 
-st.title("💬 Image Chatbot - Convert your text to Image")
-st.caption("🚀 A Streamlit image chatbot powered by Stable Diffusion")
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "What kind of image do you wish to generate?"}]
-
-for message in st.session_state.messages:
-    st.chat_message(message["role"]).write(message["content"])
-    if "image" in message:
-        st.chat_message("assistant").image(message["image"], caption=message["prompt"], use_column_width=True)
+    # app info
+if info == "About the app":
+    st.markdown(""" <style> .font {
+    font-size:30px ; font-family: 'Cooper Black'; color: black;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<h2 class="font"> About the App </h2>', unsafe_allow_html=True)
+    st.markdown(style, unsafe_allow_html= True)
+    st.markdown(about_app, unsafe_allow_html=True)
 
 
-if prompt := st.chat_input():
+elif info == "About the Developer":
+    st.markdown(""" <style> .font {
+    font-size:30px ; font-family: 'Cooper Black'; color: black;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<h2 class="font"> About the Developer </h2>', unsafe_allow_html=True)
+    st.markdown(style, unsafe_allow_html= True)
+    st.markdown(about_dev, unsafe_allow_html=True)
+    st.markdown(link, unsafe_allow_html=True)
 
-    if not st.secrets.hugging_face_token.api_key:
-        st.info("Please add your Hugging Face Token to continue.")
-        st.stop()
+elif info == "App":
+    st.title("💬 Image Chatbot - Convert your text to Image")
+    st.caption("🚀 A Streamlit image chatbot powered by Stable Diffusion")
 
-    # Input prompt
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [
+            {"role": "assistant", "content": "What kind of image do you wish to generate?"}]
 
-    # Query Stable Diffusion
-    headers = {"Authorization": f"Bearer {st.secrets.hugging_face_token.api_key}"}
-    image_bytes = query_stabilitydiff({
-        "inputs": prompt,
-    }, headers)
+    for message in st.session_state.messages:
+        st.chat_message(message["role"]).write(message["content"])
+        if "image" in message:
+            st.chat_message("assistant").image(message["image"], caption=message["prompt"], use_column_width=True)
 
-    # Return Image
-    image = Image.open(io.BytesIO(image_bytes))
-    msg = f'here is your image... "{prompt}"'
 
-    # Show Result
-    st.session_state.messages.append({"role": "assistant", "content": msg, "prompt": prompt, "image": image})
-    st.chat_message("assistant").write(msg)
-    st.chat_message("assistant").image(image, caption=prompt, use_column_width=True)
+    if prompt := st.chat_input():
+
+        if not st.secrets.hugging_face_token.api_key:
+            st.info("Please add your Hugging Face Token to continue.")
+            st.stop()
+
+        # Input prompt
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        # Query Stable Diffusion
+        headers = {"Authorization": f"Bearer {st.secrets.hugging_face_token.api_key}"}
+        image_bytes = query_stabilitydiff({
+            "inputs": prompt,
+        }, headers)
+
+        # Return Image
+        image = Image.open(io.BytesIO(image_bytes))
+        msg = f'here is your image... "{prompt}"'
+
+        # Show Result
+        st.session_state.messages.append({"role": "assistant", "content": msg, "prompt": prompt, "image": image})
+        st.chat_message("assistant").write(msg)
+        st.chat_message("assistant").image(image, caption=prompt, use_column_width=True)
+else:
+    pass
