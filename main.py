a07712a6215f3f4e6710fe50016defe7951856ec
Taoheed Oyeniyi <64612602+Taoheed-O@users.noreply.github.com>
2024-04-15 18:32:09 +0100

    new update...

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ elif info == "App":
 
     if prompt := st.chat_input():
 
-        if not st.secrets.hugging_face_token.api_key:
+        if not st.secrets["hugging_face_token_api_key"]:
             st.info("Please add your Hugging Face Token to continue.")
             st.stop()
 
